19ee0afd7f48dbcc6b864a338661b92cf38d3a54
savani-ops <girbidesavani@gmail.com>
2026-09-04 21:36:20 +0530

    Add scroll event to show sections on viewport

---
+++ script.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach((section) => {
+        const position = section.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            section.classList.add("show");
+        }
+    });
+});
